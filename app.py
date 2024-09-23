32c953cdf6f5437d72f440cce48fa8e2c6f70a15
Syed <sf.professionalwork@gmail.com>
2024-09-23 21:28:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def generate_tts():
     return send_file(audio_path, mimetype='audio/wav')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=3000)
